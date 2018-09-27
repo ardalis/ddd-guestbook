6673d825c86e2cf014dbeaee41c3638b9e9e15bb
Scott DePouw <scott@ardalis.com>
2018-09-27 09:55:53 -0400

    Completing Lab 1

---
+++ src/CleanArchitecture.Core/Entities/GuestbookEntry.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Core.SharedKernel;
+using System;
+
+namespace CleanArchitecture.Core.Entities
+{
+    public class GuestbookEntry : BaseEntity
+    {
+        public string EmailAddress { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset DateTimeCreated { get; set; } = DateTime.UtcNow;
+    }
+}
