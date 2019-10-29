9786d804a1fb202f84219dff10e08372f6c9263c
Eric Fleming <eric-fleming18@hotmail.com>
2019-10-29 11:27:18 -0400

    Completing Lab 1

---
+++ src/CleanArchitecture.Core/Entities/Guestbook.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Core.SharedKernel;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Core.Entities
+{
+    public class Guestbook : BaseEntity
+    {
+        public string Name { get; set; }
+        public List<GuestbookEntry> Entries { get; } = new List<GuestbookEntry>();
+    }
+}
